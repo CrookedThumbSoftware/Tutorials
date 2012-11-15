4a2b347c402c968f385b189fd15c3551fbf09bc7
Frank Swehosky <Frank@CrookedThumbSoftware.com>
2012-11-14 20:52:54 -0600

    Initial console app

---
+++ C-Sharp/Ch3Tutorial1/Program.cs
@@ -0,0 +1,35 @@
+﻿/**
+ * @file   Program.cs
+ * @Author Frank Swehosky (Frank@CrookedThumbSoftware.com)
+ * @date   November 2012
+ * @brief  The initial console app.
+ * Copyright (c) 2012, Crooked Thumb Software, All rights reserved.
+ * 
+ * 
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ch3Tutorial1
+{
+    /**
+     * The static class used for the initial call
+     */
+    class Program
+    {
+        /** 
+         * <summary> The main entry point for the application.</summary>
+         **/
+        static void Main(string[] args)
+        {
+            int i = 17;
+            string temp= "\"i\" is";
+            //Print to the console
+            Console.WriteLine("{0} {1}.", temp, i);
+            //Wait for the user to press a key
+            Console.ReadKey();
+        }
+    }
+}
