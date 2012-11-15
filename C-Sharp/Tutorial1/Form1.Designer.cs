da28a4785a900f5878bc26c5ea10e98261e3a2b6
Frank Swehosky <Frank@CrookedThumbSoftware.com>
2012-11-14 18:19:55 -0600

    Update gui changes per tutorial

--- C-Sharp/Tutorial1/Form1.Designer.cs
+++ C-Sharp/Tutorial1/Form1.Designer.cs
@@ -33,11 +33,12 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.button1.Location = new System.Drawing.Point(115, 16);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Click Me";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -45,7 +46,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(304, 54);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
